2eb7dab5652d95ec60e55877334643047452e88b
ZLO#DEV <59707245+MaloyMeee@users.noreply.github.com>
2023-09-20 23:36:16 +0300

    1.0.0

---
+++ settings.py
@@ -0,0 +1,15 @@
+from web3 import Web3
+from web3.middleware import geth_poa_middleware
+
+RPC_URL = "https://dapps.shardeum.org"
+web3 = Web3(Web3.HTTPProvider(RPC_URL))
+def get_private():
+    web3.eth.account.enable_unaudited_hdwallet_features()
+    account = web3.eth.account.from_mnemonic("around brother lens ...")
+    private_key = account._private_key
+    return private_key
+
+TG_BOT_KEY = "6"
+WALLET_ADDR = ""
+WALLET_PK = get_private()
+DATABASE_FILE = "faucet.db"
