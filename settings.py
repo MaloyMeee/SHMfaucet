0867294a3651067c2d3860eb0791032ec55a4931
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2023-11-07 15:55:14 +0300

    fix admin_user_id

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ from web3.middleware import geth_poa_middleware
 
 RPC_URL = "https://dapps.shardeum.org"
 web3 = Web3(Web3.HTTPProvider(RPC_URL))
-admin_user_id = ""
+admin_user_id = 
 def get_private():
     web3.eth.account.enable_unaudited_hdwallet_features()
     account = web3.eth.account.from_mnemonic("around brother lens ...")
