cd54e2080e39ba18cb317c6a8ea8567ea2ea2bef
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2023-11-07 15:54:31 +0300

    add unlimited use command for admin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import telebot
-import telegram
 from telegram.ext import *
 from web3 import Web3
 import sqlite3
@@ -11,12 +10,14 @@ from abi import TOKEN_ABI
 def main():
     createDataBase()
     print("Started")
-    bot = telegram.Bot(token=TG_BOT_KEY)
-    updater = Updater(bot=bot, use_context=True)
+    updater = Updater(TG_BOT_KEY, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("dclaim", shm_command))
+    dp.add_handler(CommandHandler("claim", shm_command))
+    dp.add_handler(CommandHandler("checkaddr", check_command))
+    dp.add_handler(CommandHandler("checkfaucetbalance", check_faucet))
     updater.start_polling()
     updater.idle()
+
 
 def createDataBase():
     if isfile(DATABASE_FILE):
@@ -29,13 +30,19 @@ def createDataBase():
         ping_admin_dm("Database created successfully")
 
 
+def get_balabce(wallet):
+    web3 = Web3(Web3.HTTPProvider(RPC_URL))
+    balance = web3.eth.get_balance(wallet)
+    return (f"Balance: {web3.from_wei(balance, 'ether')}")
+
+
 def getTokenBalance(account, token_addr):
     w3 = Web3(Web3.HTTPProvider(RPC_URL))
-    token_addr = Web3.toChecksumAddress(token_addr)
+    token_addr = Web3.to_checksum_address(token_addr)
     contractToken = w3.eth.contract(address=token_addr, abi=TOKEN_ABI)
-    account = Web3.toChecksumAddress(account)
+    account = Web3.to_checksum_address(account)
     balance = contractToken.caller().balanceOf(account)
-    return (w3.fromWei(balance, 'ether'))
+    return (w3.from_wei(balance, 'ether'))
 
 
 def sendSHM(account):
@@ -55,18 +62,29 @@ def sendSHM(account):
         return (f"Хеш транзакции: {(web3.to_hex(tx_hash))}")
     except Exception as e:
         ping_admin_dm(f"ERROR: {e}")
+        print(f"ERROR: {e}\nContinue...")
+        delete_user_from_db(account)
+        return ("Something went wrong")
+
 
 def ping_admin_dm(message):
     bot = telebot.TeleBot(TG_BOT_KEY)
     bot.send_message(admin_user_id, message)
 
 
-def isLowOnGas(wallet, user_id):
+def isLowOnGas(wallet, userId):
     web3 = Web3(Web3.HTTPProvider(RPC_URL))
     balance = web3.eth.get_balance(wallet)
-    if balance < 0.003 or user_id == admin_user_id:
+    if userId == admin_user_id:
+        return True
+    elif balance < 0.003:
         return True
     return False
+
+
+def delete_user_from_db(wallet):
+    conn = sqlite3.connect(DATABASE_FILE).cursor()
+    conn.execute("DELETE 1 FROM faucetClaims WHERE ADDR = '" + wallet + "')")
 
 
 def isEligible(userId, wallet):
@@ -74,7 +92,9 @@ def isEligible(userId, wallet):
     conn.execute("SELECT 1 FROM faucetClaims WHERE (USER_ID = " + str(
         userId) + " OR ADDR = '" + wallet + "') AND DT > julianday('now', '-24 hours') ")
     rows = conn.fetchall()
-    if (len(rows)) == 0 or userId == admin_user_id:
+    if userId == admin_user_id:
+        return True
+    elif (len(rows)) == 0:
         return True
     return False
 
@@ -99,6 +119,20 @@ def gimmeFunds(userId, address):
 def shm_command(update, context):
     context.bot.send_message(update.message.chat.id,
                              gimmeFunds(update.message.from_user['id'], update['message']['text'].split(' ')[1]),
+                             parse_mode='HTML',
+                             disable_web_page_preview=True)
+
+
+def check_command(update, context):
+    context.bot.send_message(update.message.chat.id,
+                             get_balabce(update['message']['text'].split(' ')[1]),
+                             parse_mode='HTML',
+                             disable_web_page_preview=True)
+
+
+def check_faucet(update, context):
+    context.bot.send_message(update.message.chat.id,
+                             get_balabce(WALLET_ADDR),
                              parse_mode='HTML',
                              disable_web_page_preview=True)
 
